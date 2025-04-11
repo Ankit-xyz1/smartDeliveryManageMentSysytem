b3c1f6615353acd00c8d3eb139851ba64d3700c9
Ankit-xyz1 <shahankit023@gmail.com>
2025-04-12 00:50:12 +0530

    ui builded

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,14 +1,63 @@
-import React from 'react'
+"use client"
+import { Button } from '@/components/ui/button'
+import { CalendarDays, History, Logs, Plus, Trash2, UserPen, X } from 'lucide-react';
+import React, { useState } from 'react'
 
 const page = () => {
+  const [CreateOrder, setCreateOrder] = useState<boolean>(false);
   return (
     <>
-      <div className='main h-screen md:w-[100%] w-full bg-zinc-950 flex items-center justify-center'>
-        <div className='hidden md:flex h-screen w-[1px] bg-zinc-800'></div>
-        <div className='main h-screen md:w-[60%] w-full bg-zinc-950'>
+      <div className='main1 h-screen md:w-[100%] w-full bg-zinc-950 flex items-center justify-center relative'>
+        <div className='line animate-pulse duration-500 hidden md:flex h-screen w-[1px] bg-zinc-800'></div>
+        <div className='main2 h-screen md:w-[60%] w-full bg-zinc-950 p-5'>
+          <div className="Heading-section create flex h-fit w-full justify-between items-center">
+            <div className='text-white text-xl rounded-2xl flex gap-1 justify-center items-center'><Logs />OrderList List</div>
+            <Button variant={'outline'} className='cursor-pointer' onClick={() => setCreateOrder(true)}><Plus />Create an Order</Button>
+            {CreateOrder && <>
+              <div className='h-screen w-full bg-zinc-950 absolute left-0 bottom-0 flex items-center justify-center z-10 '>
+                <div className='lines animate-pulse duration-500  hidden md:flex h-screen w-[1px] bg-zinc-800'></div>
+                <div className='h-screen md:w-[60%] w-full bg-zinc-950 p-5 relative'>
+                  <div className="absolute right-2 top-2 h-fit w-fit text-xl text-white"><Button className='cursor-pointer' onClick={() => setCreateOrder(false)}><X /></Button></div>
+                </div>
+                <div className='lines animate-pulse duration-500 hidden md:flex h-screen w-[1px] bg-zinc-800'></div>
 
+              </div>
+            </>}
+          </div>
+          <div className="metrics w-full h-[5vh] flex gap-2 items-center justify-start ">
+            <div className="active1 h-full w-[40%] flex items-center justify-center ">
+              <div className="status w-full flex gap-1 justify-start items-center">
+                <div className="blinker animate-pulse duration-1000 h-[7px] w-[7px] bg-green-500 rounded-full"></div>
+                <div className="stat-written text-white text-sm h-full w-fit flex">100 orders</div>
+              </div>
+            </div>
+          </div>
+          <div className='partners-list-box h-[90vh] w-full p-4 overflow-auto flex flex-col gap-2'>
+            <div className="IndiVidualPartner w-[100%] h-[6vh] bg-zinc-900 rounded-sm flex p-2">
+              <div className="individualname w-[50%] md:w-[30%] text-white h-full flex items-center justify-start px-1 md:text-sm text-[12px] overflow-hidden">Order number</div>
+              <div className="status w-[10%] md:w-[20%] flex gap-1 justify-start items-center">
+                <div className="blinker animate-pulse duration-1000 h-[7px] w-[7px] bg-green-500 rounded-full shadow-2xl"></div>
+                <div className="stat-written hidden md:flex text-white text-sm"> Assigned</div>
+              </div>
+              <div className='edit-delete md:w-[47%] w-[35%] flex items-center justify-end gap-2 text-black'>
+                <div><Button variant={'outline'} className='cursor-pointer'><CalendarDays /><span className='md:inline hidden'>Date</span></Button></div>
+                <div className="delete"><Button variant={'outline'} className='cursor-pointer'><History /><span className='md:inline hidden'>AssignmentHistory</span></Button></div>
+              </div>
+            </div>
+            <div className="IndiVidualPartner w-[100%] h-[6vh] bg-zinc-900 rounded-sm flex p-2">
+              <div className="individualname w-[50%] md:w-[30%] text-white h-full flex items-center justify-start px-1 md:text-sm text-[12px] overflow-hidden">order number</div>
+              <div className="status w-[10%] md:w-[20%] flex gap-1 justify-start items-center">
+                <div className="blinker animate-pulse duration-1000 h-[7px] w-[7px] bg-red-500 rounded-full shadow-2xl"></div>
+                <div className="stat-written hidden md:flex text-white text-sm"> Pending</div>
+              </div>
+              <div className='edit-delete md:w-[47%] w-[35%] flex items-center justify-end gap-2 text-black'>
+                <div><Button variant={'outline'} className='cursor-pointer'><CalendarDays /><span className='md:inline hidden'>date</span></Button></div>
+                <div className="delete"><Button variant={'outline'} className='cursor-pointer'><History /><span className='md:inline hidden'>assignment history</span></Button></div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='hidden md:flex h-screen w-[1px] bg-zinc-800'></div>
+        <div className='line animate-pulse duration-500 hidden md:flex h-screen w-[1px] bg-zinc-800'></div>
       </div>
     </>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,16 @@ export default function Home() {
     <>
       <div className='main h-screen md:w-[100%] w-full bg-zinc-950 flex items-center justify-center'>
         <div className='hidden md:flex h-screen w-[1px] bg-zinc-800'></div>
-        <div className='main h-screen md:w-[60%] w-full bg-zinc-950'>
-          
+        <div className='main h-screen md:w-[60%] w-full bg-zinc-950 flex flex-wrap p-4'>
+          <div className="w-[100%] h-full md:w-[50%] bg-red-600 p-2 flex flex-col">
+            <div className="metricsCar w-full h-[46%] bg-emerald-300"></div>
+            <div className="activeOrders metricsCar w-full h-[46%] bg-emerald-500"></div>
+          </div>
+          <div className="w-[100%] h-full md:w-[50%] bg-blue-600 p-2">
+              <div className="recentAssignments bg-green-600 h-full w-full"></div>
+          </div>
         </div>
-        <div className='hidden md:flex h-screen w-[1px] bg-zinc-800'></div>
+        <div className='hidden md:flex h-full w-[1px] bg-zinc-800'></div>
       </div>
     </>
   );
